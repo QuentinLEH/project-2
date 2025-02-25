e109d993d1373486403dd18578de6bfe02982c67
QuentinLEH <quentin.lehmann2@etu.unistra.fr>
2025-02-25 15:24:16 -0600

    Update

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
-// JS scripts placed here
+const idElement = document.getElementById('?');
+console.log('idElement',idElement);
+
+const classElement = document.getElementsByClassName('?');
+console.log('classElement',classElement);
+
+const tagElement = document.getElementsByTagName('?');
+console.log('tagElement',tagElement);
+
+
+const beforePage = document.querySelector('h1');
+console.log('beforePage',beforePage);
+beforePage.textContent = 'New Content'
+
+
+const QSElement = document.querySelector('.menu');
+console.log('QSElement',QSElement);
+
+const QSAElement = document.querySelectorAll('.menu');
+console.log('QSAElement',QSAElement);
