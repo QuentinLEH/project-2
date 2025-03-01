92959e0a2141bbeb539ff4a662dfd2203a04f2b6
QuentinLEH <quentin.lehmann2@etu.unistra.fr>
2025-03-01 08:25:44 -0600

    Huge update
    Huge update after the reste few days ago, still have to do the "about-me" page

--- js/main.js
+++ js/main.js
@@ -6,3 +6,12 @@ menuToggle.addEventListener('click', () => {
   menuToggle.setAttribute('aria-expanded',!expanded);
   menu.classList.toggle('expanded');
 });
+
+document.addEventListener("click", function (event) {
+  let checkbox = document.getElementById("menu-toggle");
+  let menu = document.querySelector(".menu-items");
+  
+  if (!menu.contains(event.target) && !checkbox.contains(event.target)) {
+      checkbox.checked = false;
+  }
+});
